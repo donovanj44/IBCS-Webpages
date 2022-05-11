8f504b4f4eb6a2dcafb09966c8a812e80846bc83
donovanj44 <josephddonovan345@gmail.com>
2022-05-11 11:04:21 -0400

    Fill in magic8Ball.js

--- 8Ball/magic8Ball.js
+++ 8Ball/magic8Ball.js
@@ -1,1 +1,28 @@
+var messages = new Array(
+    "It is certain",
+    "Without a doubt",
+    "You may rely on it",
+    "Yes definitely",
+    "It is decidedly so",
+    "As I see it, yes",
+    "Most likely",
+    "Yes",
+    "Outlook good",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Better not tell you now",
+    "Ask again later",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "Outlook not so good",
+    "My sources say no",
+    "Very doubtful",
+    "My reply is no"
+)
 
+function myFunction(){
+    var num = Math.floor(Math.random()*9);
+    var name = document.getElementById("first").value;
+    document.getElementById("response").innerHTML = name + ", your answer is " + messages[num];
+}
