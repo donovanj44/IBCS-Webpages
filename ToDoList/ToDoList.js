c7c4083caa2975ab38cdc3b3f5e9f6195fb93a91
donovanj44 <josephddonovan345@gmail.com>
2022-05-27 10:26:13 -0400

    Update ToDoList.js

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -17,9 +17,17 @@ window.addEventListener('load',()=>{
         task_content_el.innerText = task;
         task_el.appendChild(task_content_el);
         list_el.appendChild(task_el);
+        const task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerText = 'edit';
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerText = 'delete';
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+        task_el.appendChild(task_actions_el);
+        list_el.appendChild(task_el);
     })
 });
-function MyFunction(){
-
-    
-}
