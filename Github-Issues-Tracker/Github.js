29adc8da011019b148ff1c9b557ffb059969dc50
donovanj44 <josephddonovan345@gmail.com>
2022-06-03 10:34:26 -0400

    Update variable names in Github-Issues-Tracker

--- Github-Issues-Tracker/Github.js
+++ Github-Issues-Tracker/Github.js
@@ -1,52 +1,52 @@
 window.addEventListener('load',()=>{
     const form = document.querySelector("#new-issue-form");
     const input = document.querySelector("#new-issue-input");
-    const list_el = document.querySelector("#tasks");
+    const list_el = document.querySelector("#issues");
     form.addEventListener('submit',(e)=>{
         e.preventDefault();
-        const task = input.value;
-        console.log(task);
-        if(!task){
-            alert("fill out the task idiot");
+        const issue = input.value;
+        console.log(issue);
+        if(!issue){
+            alert("fill out the issue idiot");
             return;
         }
-        const task_el = document.createElement('div');
-        task_el.classList.add("task");
-        const task_content_el = document.createElement('div');
-        task_content_el.classList.add('content');
-        task_el.appendChild(task_content_el);
-        const task_input_el=document.createElement('input');
-        task_input_el.classList.add('text');
-        task_input_el.type = 'text';
-        task_input_el.value = task;
-        task_input_el.setAttribute('readonly','readonly');
-        task_content_el.appendChild(task_input_el);
-        const task_actions_el = document.createElement('div');
-        task_actions_el.classList.add('actions');
-        const task_edit_el = document.createElement('button');
-        task_edit_el.classList.add('edit');
-        task_edit_el.innerText = 'edit';
-        const task_delete_el = document.createElement('button');
-        task_delete_el.classList.add('delete');
-        task_delete_el.innerText = 'delete';
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-        task_el.appendChild(task_actions_el);
-        list_el.appendChild(task_el);
-        task_edit_el.addEventListener('click',(e)=>{
-            if(task_edit_el.innerText.toLowerCase()=='edit'){
-                task_edit_el.innerText="SAVE";
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
+        const issue_el = document.createElement('div');
+        issue_el.classList.add("issue");
+        const issue_content_el = document.createElement('div');
+        issue_content_el.classList.add('content');
+        issue_el.appendChild(issue_content_el);
+        const issue_input_el=document.createElement('input');
+        issue_input_el.classList.add('text');
+        issue_input_el.type = 'text';
+        issue_input_el.value = issue;
+        issue_input_el.setAttribute('readonly','readonly');
+        issue_content_el.appendChild(issue_input_el);
+        const issue_actions_el = document.createElement('div');
+        issue_actions_el.classList.add('actions');
+        const issue_comment_el = document.createElement('button');
+        issue_comment_el.classList.add('comment');
+        issue_comment_el.innerText = 'comment';
+        const issue_close_el = document.createElement('button');
+        issue_close_el.classList.add('close');
+        issue_close_el.innerText = 'close';
+        issue_actions_el.appendChild(issue_comment_el);
+        issue_actions_el.appendChild(issue_close_el);
+        issue_el.appendChild(issue_actions_el);
+        list_el.appendChild(issue_el);
+        issue_comment_el.addEventListener('click',(e)=>{
+            if(issue_comment_el.innerText.toLowerCase()=='comment'){
+                issue_comment_el.innerText="SAVE";
+                issue_input_el.removeAttribute("readonly");
+                issue_input_el.focus();
             }
             else{
-                task_edit_el.innerText="Edit";
-                task_input_el.setAttribute('readonly','readonly');      
+                issue_comment_el.innerText="Edit";
+                issue_input_el.setAttribute('readonly','readonly');      
             }
         });
-        task_delete_el.addEventListener('click',(e)=>{
-            if(task_delete_el.innerText.toLowerCase()=='delete'){
-                task_el.remove();
+        issue_close_el.addEventListener('click',(e)=>{
+            if(issue_close_el.innerText.toLowerCase()=='close'){
+                issue_el.remove();
             }
         });
     });
